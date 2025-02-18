35675c5031a1460b74555673954a76b2ef4ccbae
ShailySarker <shailysarker99@gmail.com>
2025-02-18 23:40:08 +0600

    Header added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ import { IoFlash } from "react-icons/io5";
 const Footer = () => {
     return (
         <div>
-            <div className=" bg-[#A21D3C] text-white xl:py-12 md:py-8 py-6 lg:flex lg:flex-row lg:justify-between grid md:grid-cols-2 grid-cols-1 lg:gap-0 md:gap-x-24 md:gap-y-14 gap-6 lg:px-12 md:px-7 px-5">
+            <div className=" bg-[#A21D3C] text-white xl:py-12 md:py-8 py-6 lg:flex lg:flex-row lg:justify-between grid md:grid-cols-2 grid-cols-1 lg:gap-0 md:gap-x-24 md:gap-y-14 gap-6 xl:px-14 lg:px-12 md:px-7 px-5">
                 <div className="md:mt-2 flex flex-col md:items-start items-center xl:w-[22%] lg:w-[25%]">
                     <h2 className="font-bold text-white xl:text-3xl md:text-2xl text-xl flex items-center gap-1"><IoFlash />Post View</h2>
                     <div className="xl:mt-6 md:mt-5 mt-4">
@@ -47,7 +47,7 @@ const Footer = () => {
                 </div>
             </div>
             <div className='bg-black xl:py-4 md:py-3 py-2'>
-                <p className='font-medium text-[#FFFFFF] text-center xl:text-base md:text-sm text-[13px] flex items-center gap-1 justify-center'>© 2024 <span className='font-bold flex items-center gap-[2px]'><IoFlash />Post View</span>. All Rights Reserved.</p>
+                <p className='font-medium text-[#FFFFFF] text-center xl:text-base md:text-sm text-[13px] flex items-center gap-1 justify-center'>© <span>{moment().format('YYYY')}</span><span className='font-bold flex items-center gap-[2px]'><IoFlash />Post View</span>. All Rights Reserved.</p>
             </div>
         </div>
     )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import { IoFlash } from "react-icons/io5";
 
 const Header = () => {
     return (
-        <div>
-            
+        <div className="sticky top-0 z-50 border-b-2 bg-white border-[#A21D3C] xl:px-24 lg:px-16 md:px-10 px-5 xl:py-6 lg:py-[14px] md:py-4 py-[14px] flex justify-between">
+            <h2 className="font-bold text-[#A21D3C] xl:text-3xl md:text-2xl text-xl flex items-center gap-1"><IoFlash />Post View</h2>
+            <div className="flex items-center gap-3">
+                <button className='bg-[#A21D3C] xl:py-[10px] lg:py-2 md:py-[6px] py-1 text-white rounded-3xl shadow-md px-4 font-semibold lg:text-base md:text-sm text-[13.8px] lg:w-28 w-24'>Sign Up</button>
+                <button className='hidden md:visible border-2 border-[#A21D3C] xl:py-[10px] lg:py-2 md:py-[6px] py-1 text-[#A21D3C] rounded-3xl shadow-md px-4 font-semibold lg:text-base md:text-sm text-[13.8px] lg:w-28 w-24'>Sign In</button>
+            </div>
         </div>
     );
 };
